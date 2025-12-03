1e591ff47908fa8cbb10cc0be3d66f495642bdeb
KeinerMD <kalethmoralesduarte05@gmail.com>
2025-12-03 15:43:17 -0500

    fix: btn max

---
+++ wldAbi.js
@@ -0,0 +1,10 @@
+export const WLD_ABI = [
+  {
+    "constant": true,
+    "inputs": [{ "name": "account", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "name": "", "type": "uint256" }],
+    "type": "function",
+    "stateMutability": "view"
+  }
+];
